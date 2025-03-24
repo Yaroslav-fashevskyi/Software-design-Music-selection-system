dc425afe6489a04e5abfc1685e088f3ab86c19c9
Yaroslav <yaroslav.i.fashevskyi@ukd.edu.ua>
2025-03-24 10:44:17 +0200

    практична 9

--- Music_selection_system/polls/views.py
+++ Music_selection_system/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Question
+
+def question_list(request):
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'question_list.html', context)
+
 
 # Create your views here.
