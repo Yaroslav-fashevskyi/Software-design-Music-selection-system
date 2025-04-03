85532e73b4129ce904602c5ce956ad32ddf7659c
Yaroslav <yaroslav.i.fashevskyi@ukd.edu.ua>
2025-04-03 08:52:45 +0300

    done

--- Music_selection_system/polls/urls.py
+++ Music_selection_system/polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import register_view
+
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', views.question_list, name='question_list'),
+    path('register/', register_view, name='register'),
 ]
 
--- Music_selection_system/polls/views.py
+++ Music_selection_system/polls/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 
 def question_list(request):
     questions = Question.objects.all()
     context = {'questions': questions}
     return render(request, 'question_list.html', context)
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Після успішного збереження, одразу авторизуємо користувача
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
 
 
 # Create your views here.
