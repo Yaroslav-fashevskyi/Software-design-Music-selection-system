859d9195b9bc2ed31577bbccfc7d3fdb29b3cfe9
Yaroslav <yaroslav.i.fashevskyi@ukd.edu.ua>
2025-04-07 09:54:20 +0300

    iyp

--- Music_selection_system/polls/urls.py
+++ Music_selection_system/polls/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from . import views
-from .views import register_view
+
 
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', views.question_list, name='question_list'),
-    path('register/', register_view, name='register'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
+
+
 
--- Music_selection_system/polls/views.py
+++ Music_selection_system/polls/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Question
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from .forms import UserRegistrationForm
+from .forms import UserLoginForm
 
 def question_list(request):
     questions = Question.objects.all()
     context = {'questions': questions}
     return render(request, 'question_list.html', context)
-
-
 
 def register_view(request):
     if request.method == 'POST':
@@ -17,11 +18,28 @@ def register_view(request):
             user = form.save()
             # Після успішного збереження, одразу авторизуємо користувача
             login(request, user)
-            return redirect('http://127.0.0.1:8000/polls/')
+            return redirect(reverse('question_list'))
     else:
         form = UserRegistrationForm()
-
     return render(request, 'register.html', {'form': form})
 
+def login_view(request):
+    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('question_list'))
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Повертаємо користувача на сторінку логіну (або на будь-яку іншу)
 
 # Create your views here.
